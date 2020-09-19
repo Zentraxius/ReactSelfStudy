57de26fe6c107a7cc26f5c29020c2a36bdf7f0d8
Cody Fritz <clanalia55@gmail.com>
2020-09-19 13:42:54 -0700

    commit prior to running npm run build

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.css";
 import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
 import AddTodo from "./components/AddTodo";
 import About from "./components/pages/About";
-import { v4 as uuidv4 } from "uuid";
+// import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
   state = {
     todos: [
-      {
-        title: "Take out the trash",
-        id: uuidv4(),
-        completed: false,
-      },
-      {
-        title: "Run dishes",
-        id: uuidv4(),
-        completed: true,
-      },
-      {
-        title: "Squish the cat",
-        id: uuidv4(),
-        completed: false,
-      },
-    ],
+    //   {
+    //     title: "Take out the trash",
+    //     id: uuidv4(),
+    //     completed: false,
+    //   },
+    //   {
+    //     title: "Run dishes",
+    //     id: uuidv4(),
+    //     completed: true,
+    //   },
+    //   {
+    //     title: "Squish the cat",
+    //     id: uuidv4(),
+    //     completed: false,
+    //   },
+    // 
+  ]
   };
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    .then(res => this.setState({todos: res.data}))
+  }
 
   // Toggles Complete!
   markComplete = (id) => {
@@ -40,24 +47,65 @@ class App extends Component {
     });
     // console.log(id)
   };
+///////////////////////////////////////////////////////////////////////
+// Delete the Todo Item using Axios/Post
+delTodo = (id) => {
+  axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+  .then(res =>this.setState({ todos: [...this.state.todos.filter
+    (todo => todo.id !== id)] }));
+}
 
-  // Delete the Todo Item
-  delTodo = (id) => {
-    this.setState({
-      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
-    });
-    // console.log(id)
-  };
+///////////////////////////////////////////////////////////////////////
+  // // Delete the Todo Item || Original
+  // delTodo = (id) => {
+  //   this.setState({
+  //     todos: [...this.state.todos.filter
+  //     ((todo) => todo.id !== id)],
+  //   });
+  //   // console.log(id)
+  // };
+///////////////////////////////////////////////////////////////////////
 
-  // Add Todo
-  addTodo = (title) => {
-    const newTodo = {
-      id: uuidv4(),
-      title: title,
-      completed: false,
-    };
-    this.setState({ todos: [...this.state.todos, newTodo] });
-  };
+
+// Happy Space
+
+///////////////////////////////////////////////////////////////////////
+  // Add Todo with API/Posting using Axios                           //
+  addTodo = (title) => {                                             //
+    axios.post('https://jsonplaceholder.typicode.com/todos', {title, //
+    completed: false                                                 //
+  })                                                                 //
+  .then(res => this.setState({ todos:                                //
+    [...this.state.todos, res.data] }));                             //
+    // const newTodo = {                                             //
+    //   // id: uuidv4(),                                            //
+    //   title: title,                                               //
+    //   completed: false,                                           //
+    // };                                                            //
+    // this.setState({ todos: [...this.state.todos, newTodo] });     //
+  };                                                                 //
+///////////////////////////////////////////////////////////////////////
+    // Add Todo || Original Add Todo
+
+    // addTodo = (title) => {
+    //   const newTodo = {
+    //     // id: uuidv4(),
+    //     title: title,
+    //     completed: false,
+    //   };
+    //   this.setState({ todos: [...this.state.todos, newTodo] });
+    // };
+/////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
   render() {
     return (
       <Router>
