684fcd44b20b9cb41a02e0eccc8d0f17e238ee4d
Cody Fritz <clanalia55@gmail.com>
2020-09-19 13:22:53 -0700

    make commit prior to installing axios for API testing and HTTP requests

--- src/App.js
+++ src/App.js
@@ -1,67 +1,83 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
-import Todos from './components/Todos';
-import Header from './components/layout/Header'
-import AddTodo from './components/AddTodo';
-import { v4 as uuidv4 } from 'uuid';
+import Todos from "./components/Todos";
+import Header from "./components/layout/Header";
+import AddTodo from "./components/AddTodo";
+import About from "./components/pages/About";
+import { v4 as uuidv4 } from "uuid";
 
 class App extends Component {
-
   state = {
     todos: [
       {
-        title: 'Take out the trash',
+        title: "Take out the trash",
         id: uuidv4(),
-        completed: false
+        completed: false,
       },
       {
-        title: 'Run dishes',
+        title: "Run dishes",
         id: uuidv4(),
-        completed: true
+        completed: true,
       },
       {
-        title: 'Squish the cat',
+        title: "Squish the cat",
         id: uuidv4(),
-        completed: false
-      }
-    ]
-  }
+        completed: false,
+      },
+    ],
+  };
 
   // Toggles Complete!
   markComplete = (id) => {
-    this.setState( { todos: this.state.todos.map(todo => {
-      if(todo.id === id) {
-        todo.completed = !todo.completed;
-      }
-      return todo;
-    }) });
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    });
     // console.log(id)
-  }
+  };
 
-// Delete the Todo Item
-delTodo = (id) => {
-  this.setState({todos:[...this.state.todos.filter(todo=>todo.id!==id)]});
-  // console.log(id)
-}
+  // Delete the Todo Item
+  delTodo = (id) => {
+    this.setState({
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+    });
+    // console.log(id)
+  };
 
   // Add Todo
   addTodo = (title) => {
     const newTodo = {
       id: uuidv4(),
       title: title,
-      completed: false
-    }
-    this.setState({ todos: [...this.state.todos, newTodo]});
-  }
+      completed: false,
+    };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodo addTodo={this.addTodo} />
-          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/"
+              render={(props) => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}/>
+                </React.Fragment>
+              )}/>
+              <Route path="/about" component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This page is reached at localhost:3000/about and is part of a project created for self-study.</p>
+    </React.Fragment>
+  )
+}
+
+export default About;
